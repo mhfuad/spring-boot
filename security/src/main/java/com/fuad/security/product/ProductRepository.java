df5a2211e9df735f8ab30746f4c529cb331aaec2
mhfuad <mhfuad2@gmail.com>
2023-05-16 15:34:02 +0600

    jpa added

--- security/src/main/java/com/fuad/security/product/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fuad.security.product;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-    Page<ProductEntity> findAll(Pageable pageable);
-}
