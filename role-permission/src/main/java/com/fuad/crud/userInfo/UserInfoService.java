1ded282c7805a78dfd6493c5eb44bbfbf6f68cd0
mhfuad <mhfuad2@gmail.com>
2023-05-25 10:37:07 +0600

    role-permission project added

---
+++ role-permission/src/main/java/com/fuad/crud/userInfo/UserInfoService.java
@@ -0,0 +1,28 @@
+package com.fuad.crud.userInfo;
+
+import com.fuad.crud.userInfo.UserInfo;
+import com.fuad.crud.userInfo.UserInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserInfoService {
+
+    @Autowired
+    private UserInfoRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public String saveUser(UserInfo userInfo){
+        userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+        repository.save(userInfo);
+        return "User added to system";
+    }
+
+    public UserInfo findByUsername(String username) {
+        return repository.findByName(username).get();
+    }
+}
